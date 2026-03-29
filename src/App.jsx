975c489ee2e7b0c31fa8c3aaaad04aaae93ee7e2
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-03-29 21:51:17 +0600

    Set up project and added source files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-400 text-center mt-10">
+        Hello world!
+      </h1>
+      {/* name of each tab group should be unique */}
+
+    </>
+  )
+}
+
+export default App
