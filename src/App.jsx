a077ba9ef6c5826c85b845ebf56bfccf47d21af9
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-03-29 23:14:47 +0600

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Navbar from './component/Navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
+    <Navbar/>
       <h1 className="text-3xl font-bold text-red-400 text-center mt-10">
         Hello world!
       </h1>
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import ShoppingCard from '../../assets/shopping-cart.png';
+
+const Navbar = () => {
+    return (
+        <nav>
+<div className="max-w-7xl mx-auto navbar justify-between">
+  <div >
+    <a className="text-4xl font-bold bg-linear-to-b from-[rgba(79,57,246,1)] to-[rgba(149,20,250,1)] bg-clip-text text-transparent">DigiTools</a>
+  </div>
+  <div className='text-[#101727]'>
+    <ul className="flex gap-4 menu menu-horizontal px-1">
+        <li><a>Products</a></li>
+        <li><a>Features</a></li>
+        <li><a>Pricing</a></li>
+        <li><a>Testimonials</a></li>
+        <li><a>FAQ</a></li>
+    </ul>
+  </div>
+  <div className="flex space-x-4">
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <div className="indicator">
+            <img className='w-[24px]' src={ShoppingCard} alt="Shopping-Cart" />
+            <span className="badge  indicator-item rounded-full  bg-red-500 text-sm text-white py-1 px-2">8</span>
+        </div>
+      </div>
+      <div
+        tabIndex={0}
+        className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+        <div className="card-body">
+          <span className="text-lg font-bold">8 Items</span>
+          <span className="text-info">Subtotal: $999</span>
+          <div className="card-actions">
+            <button className="btn btn-primary btn-block">View cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="flex gap-2">
+      <button>Login</button>
+      <button className='btn btn-primary bg-gradient-to-r from-[rgba(79,57,246,1)] to-[rgba(149,20,250,1)] text-white rounded-3xl'>Get Started</button>
+    </div>
+  </div>
+</div>
+        </nav>
+    );
+};
+
+export default Navbar;
