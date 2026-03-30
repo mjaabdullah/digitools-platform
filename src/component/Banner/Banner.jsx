83b91cae47f9e9c178a9e36eba6461e5a4c5f91d
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-03-30 13:52:14 +0600

    Fixed banner seciton for mobile devices

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -2,8 +2,8 @@ import BannerImg from "../../assets/banner.png";
 import CircleImg from "../../assets/circle.png";
 const Banner = () => {
     return (
-<div className="max-w-7xl mx-auto my-16">
-    <div className="hero-content flex-col lg:flex-row-reverse justify-between">
+<div className="">
+    <div className="my-16 hero-content mx-auto flex-col lg:flex-row-reverse lg:justify-between justify-center items-center">
     <img
       src={BannerImg}
       className="max-w-sm rounded-lg shadow-sx"
@@ -13,13 +13,13 @@ const Banner = () => {
             <img className="w-4" src={CircleImg} alt="Circle" />
             <span className="bg-linear-to-b from-[rgba(79,57,246,1)] to-[rgba(149,20,250,1)] bg-clip-text text-transparent">New: AI-Powered Tools Available</span>
             </div>
-      <h1 className="text-7xl/20 font-extrabold text-[#101727]">Supercharge Your
+      <h1 className="text-3xl md:text-7xl/20 font-extrabold text-[#101727]">Supercharge Your
         
         Digital Workflow</h1>
       <p className="text-[#627382]">
         Access premium AI tools, design assets, templates, and productivity software—all in one place. Start creating faster today.Explore Products
       </p>
-      <div className="space-x-2">
+      <div className="flex gap-2 flex-wrap">
         <button className='btn btn-primary bg-gradient-to-r from-[rgba(79,57,246,1)] to-[rgba(149,20,250,1)] text-white rounded-3xl'>Explore Products</button>
       <button className='btn btn-outline btn-primary rounded-3xl'>Explore Products</button>
       </div>
